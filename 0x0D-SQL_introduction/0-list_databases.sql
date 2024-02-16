b704f0e1eb9fe6f0f33c06e6209e77e57a3a9e03
Senu-sesi2 <144575546+Senu-sesi2@users.noreply.github.com>
2024-02-16 19:02:50 +0000

    Update 0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
--- List all databases of your MySQL server
-=======
--- Script that lists all databases of your MySQL server.
->>>>>>> 8bfddffdfbc3c4bb47e12481f73539d2d946ab88
+-- Script that lists all databases of your MySQL server
 SHOW DATABASES;

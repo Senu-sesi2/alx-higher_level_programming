059455624988de48e5ca505d5f321c307e2257be
Senu-sesi2 <senu.sesi2@gmail.com>
2024-03-17 20:08:22 +0000

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-"""
+"""Lists states"""
+
 import MySQLdb
 from sys import argv
 

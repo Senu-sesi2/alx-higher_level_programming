a244f7a123519bf48b837fdc574ab3a00f7b80b4
Senu-sesi2 <senu.sesi2@gmail.com>
2024-03-28 23:02:33 +0000

    upload file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Script that fetches https://alx-intranet.hbtn.io/status using requests
+Script that fetches https://intranet.hbtn.io/status
 """
-
 import requests
 
-
-if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
+if __name__ == '__main__':
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))

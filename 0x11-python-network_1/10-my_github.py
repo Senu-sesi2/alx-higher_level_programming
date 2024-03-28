09285b2b7fae34b3a14a7b51897408fef1a001be
Senu-sesi2 <senu.sesi2@gmail.com>
2024-03-28 23:19:43 +0000

    upload file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
 """
 import requests
 from requests.auth import HTTPBasicAuth

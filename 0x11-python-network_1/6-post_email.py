83965f3ae7ec370a240f40f505ee34f9d475da40
Senu-sesi2 <senu.sesi2@gmail.com>
2024-03-28 23:08:50 +0000

    upload file

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""Takes a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter and finally displays the body of the response
 """
-Script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response
-"""
-import requests
-from sys import argv
 
+import sys
+import requests
 
 if __name__ == "__main__":
-    url = argv[1]
-    value = {"email": argv[2]}
-    req = requests.post(url, data=value)
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
 
-    print(req.text)
+    response = requests.post(sys.argv[1], data=email)
+    print(response.text)

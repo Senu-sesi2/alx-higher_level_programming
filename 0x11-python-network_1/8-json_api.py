99abbec5679812150888901c5831cfefb124a30a
Senu-sesi2 <senu.sesi2@gmail.com>
2024-03-28 23:15:44 +0000

    upload file

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter
 """
-
-import sys
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
 import requests
+from sys import argv
 
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        letter = ""
-    else:
-        letter = sys.argv[2]
-
-    payload = {'q': letter}
-
-    request = requests.post('http://0.0.0.0:5000/search_user', data=payload)
-
+if __name__ == '__main__':
+    q = argv[1] if len(argv) == 2 else ""
+    url = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url, data={'q': q})
     try:
-        response = request.json()
-        if response == {}:
-            print('No result')
+        r_dict = r.json()
+        id, name = r_dict.get('id'), r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
         else:
-            print('[{}] {}'.format(response.get('id'), response.get('name')))
-    except ValueError:
-        print('Not a valid JSON')
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except Exception:
+        print("Not a valid JSON")

b11de194975ab9717b3981da05f2a8b12ee8b08e
Senu-sesi2 <senu.sesi2@gmail.com>
2024-04-23 16:00:51 +0000

    a script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-a script that display the status code of a GET request.
+// a script that display the status code of a GET request.
 const request = require('request');
 
 request(process.argv[2], function (_err, res) {

bea6165d9f1bd58f5a395de52be593cc795239f0
Senu-sesi2 <senu.sesi2@gmail.com>
2024-04-23 15:55:38 +0000

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// a script that prints the title of a Star Wars movie where the episode number matches a given integer.
+const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
+});
